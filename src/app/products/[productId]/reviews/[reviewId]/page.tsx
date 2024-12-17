5eb1b4e900ec31b46925947f2043b7254cb808aa
Praveen1404 <praveen1404@live.com>
2024-12-17 15:27:26 +0530

    till class Handling errors

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,28 @@
+"use client";
 import { notFound } from "next/navigation";
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
 export default function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
-  if(parseInt(params.reviewId) > 1000) {
+  // const random = getRandomInt(2);
+  // if (random === 1) {
+  //   throw new Error('Error loading review')
+  // }
+
+  if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
-  return <h1>Review {params.reviewId} for product {params.productId} </h1>;
+  return (
+    <h1
+      style={{
+        backgroundColor: "red",
+      }}
+    >
+      Review {params.reviewId} for product {params.productId}{" "}
+    </h1>
+  );
 }
